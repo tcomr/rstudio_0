6d0fc8c0ee8440e9c999b523974cbec65a82495b
tcomr <tcomraaee@gmail.com>
2021-03-10 02:57:08 -0500

    updates with matrix

--- r0_basic_R.R
+++ r0_basic_R.R
@@ -42,9 +42,42 @@ good_day <- my_vec[pos_day]
 good_day
 
 
+#====================matrix================
+matrix(1:20, nrow = 4, byrow = TRUE)
+matrix(1:20, nrow = 4, byrow = FALSE)
 
+her_me <- c(my_vec, her_vec)
+her_me
 
+hm_mat_tall <- matrix(her_me)
+hm_mat_tall
 
+hm_wide <- matrix(her_me, nrow = 1)
+hm_wide
 
+hm_default <- matrix(her_me, nrow = 2)
+hm_default
+
+hm_byrow <- matrix(her_me, nrow = 2, byrow =  TRUE)
+hm_byrow
+
+# rownames, colnames 
+big_mat <- t(hm_byrow) # transpose matrix
+big_mat
+rownames(big_mat) <- day_vec
+big_mat
+
+colnames(big_mat) <- c('Bowl', 'balance')
+big_mat
+help(matrix)
+
+d_mat <- matrix(her_me, nrow = 2, byrow = TRUE, 
+                dimnames = list(c('a', 'b'), list('ab', 'cd', 'ef', 'gh', 'ij')))
+d_mat # if only one arguement in dimnames, it is rownames 
+
+# cbind(), rbind(), colSums()
+# head(), matrix[, 2]
+# modulus %%; 
+5%%3 # 2
 
 
